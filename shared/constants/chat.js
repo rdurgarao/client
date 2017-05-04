69d9239c5ef5cf4071f87e5c2021619927c773bc
Steve Sanders <steve@keyba.se>
2017-05-03 18:34:14 -0700

    wip

--- shared/constants/chat.js
+++ shared/constants/chat.js
@@ -756,7 +756,7 @@ const getSupersedes = (state: TypedState): ?SupersedeInfo => {
 }
 
 function isImageFileName (filename: string): boolean {
-  return filename.match(/[^/]+\.(jpg|png|gif|jpeg|bmp)$/) == null
+  return filename.match(/[^/]+\.(jpg|png|gif|jpeg|bmp)$/) != null
 }
 
 export {
